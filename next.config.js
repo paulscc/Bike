adc145552a43bc07fa93fc09faf5269cad8b5d12
paulscc <paul.sc9@hotmail.com>
2026-04-14 11:05:39 -0500

    oks

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
@@ -85,6 +86,6 @@ const nextConfig = {
 };
 
 module.exports = {
-  output:'export',
-
+  ...nextConfig,
+  output: 'export',
 };
